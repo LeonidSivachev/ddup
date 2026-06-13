60596620ffeff4dec84fd4622bf48464bb4b50d7
Leonid Sivachev <sivachev.ld@phystech.edu>
2026-06-13 19:12:58 +0300

    Implement initial design of test_backup function.
    It suggests using an external flash device for testing
    and creates its image. Verification should be added in
    the future, currently tested "by hands".
    
    Signed-off-by: Leonid Sivachev <sivachev.ld@phystech.edu>

--- tests/test_backup.c
+++ tests/test_backup.c
@@ -1,17 +1,38 @@
-#include <fcntl.h>
-#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "backup.h"
+#include "parse.h"
 #include "tests.h"
 
-int test_get_size()
+#define DISK_NAME_SIZE   32
+#define BACKUP_FILE_NAME "backup.img"
+
+void test_backup()
 {
-  /*
+  if (geteuid() != 0)
+  {
+    printf("Running this test requires \'sudo\'!\n");
+    return;
+  }
+
+  printf("Enter the disk for testing: ");
   printf(
-      "Note: Running this test requires 'sudo'.\n
-      To test 'get_size()' function type the name
-      of disk"
-  )
-  */
+      "(example: /dev/sdb, the best option is using external flash device)\n");
+  printf("> ");
+
+  char disk_name[DISK_NAME_SIZE];
+  fgets(disk_name, DISK_NAME_SIZE, stdin);
+  disk_name[strcspn(disk_name, "\n")] = '\0';
+  /*
+   * NOTE: I don't like it, seems like creating global config is bad idea.
+   * Maybe in I should fix it.
+   */
+  gconfig.bs = DEFAULT_BS;
+
+  printf("Backing up %s ...\n", disk_name);
+  backup_disk(disk_name, BACKUP_FILE_NAME);
+  printf("Backup of %s completed successfully.\n", disk_name);
+
+  /* TODO: Implement verification of image. */
 }
