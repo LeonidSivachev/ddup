5a299cb9dc291e472ddb5d98cf2595bd185eaf53
Leonid Sivachev <sivachev.ld@phystech.edu>
2026-06-21 18:23:36 +0300

    Improve test for progress bar.
    Now the test emulates two processes of copying,
    making it closer to real usage of progress bar and
    providing more opportunity to catch bugs.
    
    Signed-off-by: Leonid Sivachev <sivachev.ld@phystech.edu>

--- tests/test_prog_bar.c
+++ tests/test_prog_bar.c
@@ -1,14 +1,27 @@
 #include <backup.h>
 #include <signal.h>
+#include <stdio.h>
 #include <unistd.h>
+
+#define TEST_FREQ 5
 
 void test_prog_bar()
 {
   signal(SIGWINCH, sigwinch_handler);
 
-  for (int i = 0; i <= 100; i += 5)
+  printf("First copying...\n");
+  for (int i = 0; i <= 100; i += TEST_FREQ)
   {
     print_progress(i, 100);
     sleep(1);
   }
+  printf("First copying completed.\n");
+
+  printf("Second copying...\n");
+  for (int i = 0; i <= 100; i += TEST_FREQ)
+  {
+    print_progress(i, 100);
+    sleep(1);
+  }
+  printf("Second copying completed.\n");
 }
